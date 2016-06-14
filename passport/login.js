85c37cf868ffab436397347b6b2e1e8de1f69f24
Paul van Motman <pcvanmotman@gmail.com>
2016-06-14 16:51:47 +0200

    finished passport login and registration

--- passport/login.js
+++ passport/login.js
@@ -1,4 +1,4 @@
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var db = require('../app/models/database');
 var bCrypt = require('bcrypt');
 
@@ -8,26 +8,26 @@ module.exports = function(passport){
             passReqToCallback : true
         },
         function(req, username, password, done) { 
-            // check in database if a user with username exists or not
-            db.mainuser.findOne({ where: {'username':  username} }).then(function(user, err){
-                // In case of any error, return using the done method
-                if (err) {
-                    return done(err);
-                }
-                // Username does not exist, log the error and redirect back
-                if (!user){
-                    console.log('User Not Found with username '+ username);
-                    return done(null, false, req.flash('message', 'User Not found.'));                 
-                }
-                // User exists but wrong password, log the error 
-                if (!isValidPassword(user, password)){
-                    console.log('Invalid Password');
-                    return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
-                }
-                // User and password both match, return user from done method
-                // which will be treated like success
-                return done(null, user);
-            })
+        	db.mainuser.find({ where: {'username' :  username }}).then(
+        		function(user) {
+        			if (!user){
+                        console.log('User Not Found with username '+username);
+                        return done(null, false, req.flash('message', 'User Not found.'));                 
+                    }
+                    // User exists but wrong password, log the error 
+                    if (!isValidPassword(user, password)){
+                        console.log('Invalid Password');
+                        return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                    }
+                    // User and password both match, return user from done method
+                    // which will be treated like success
+                    console.log('found user')
+                    return done(null, user);
+                },
+                function(err) {
+                	return done(err);
+                });
+
         })
     );
 
