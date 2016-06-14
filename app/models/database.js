8420b4d444562796ed0e2da27235ed0177c8f29f
Paul van Motman <pcvanmotman@gmail.com>
2016-06-14 10:40:04 +0200

    finished database module and mainuser model

---
+++ app/models/database.js
@@ -0,0 +1,36 @@
+// Container object
+var db = {
+	mod: {}
+}
+
+// Set up sql
+var Sequelize = require( 'sequelize' )
+db.conn = new Sequelize('guesty', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+});
+
+
+//// Models
+// Users
+db.mainuser = db.conn.define( 'mainuser', {
+	firstname: Sequelize.STRING,
+	lastname: Sequelize.STRING,
+	email: Sequelize.STRING,
+	organisation: Sequelize.STRING,
+	username: Sequelize.STRING,
+	password: Sequelize.STRING,
+	telephone: Sequelize.STRING,
+	location: Sequelize.STRING
+})
+
+
+
+
+// Synchronise with database
+db.conn.sync(  ).then( 
+	() => { console.log ( 'Sync succeeded' ) },
+	( err ) => { console.log('sync failed: ' + err) } 
+	)
+
+module.exports = db
