51b231b794b35cb0fc917c237aa6a3754caba510
Paul van Motman <pcvanmotman@gmail.com>
2016-06-14 16:51:47 +0200

    finished passport login and registration

--- passport/init.js
+++ passport/init.js
@@ -2,20 +2,17 @@ var login = require('./login');
 var signup = require('./signup');
 var db = require('../app/models/database');
 
-
 module.exports = function(passport){
-
-	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    // Serialize sessions
     passport.serializeUser(function(user, done) {
-        console.log('serializing user: ');console.log(user);
-        done(null, user.id);
+    	done(null, user.id);
     });
 
     passport.deserializeUser(function(id, done) {
-        db.mainuser.findById(id, function(err, user) {
-            console.log('deserializing user:',user);
-            done(err, user);
-        });
+    	db.mainuser.find({where: {id: id}}).then(
+    		function(user){ done(null, user) },
+    		function(err){ done(err, null) }
+    	);
     });
 
     // Setting up Passport Strategies for Login and SignUp/Registration
