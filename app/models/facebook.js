6d20ec77a1e704404f0d297fc878b48fda806fd4
Sven van Hoorn <svenvanhoorn@gmail.com>
2016-06-16 11:28:51 +0200

    FB login + FB api accesstoken working

--- app/models/facebook.js
+++ app/models/facebook.js
@@ -6,21 +6,17 @@ passport.use(new Strategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/login/facebook/return',
-    profileFields: ['id', 'name', 'displayName', 'photos', 'emails', 'events', 'cover']
+    profileFields: ['id', 'name', 'displayName', 'photos', 'events', 'emails']
   },
   function(accessToken, refreshToken, profile, cb) {
-  	console.log("FACEBOOK PASSPORT")
-
-  	// console.log(profile.events)
-
+  	console.log("#### PASSPORT FACEBOOK ####")
   	// console.log(profile._json.events)
-  	console.log('###############################################################')
+  	console.log('########### AccessToken ################')
+    console.log(accessToken)
+    console.log("#### END ####")
 
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
+    profile.accessToken = accessToken;
+
     return cb(null, profile);
   }));
 
--- routes/index.js
+++ routes/index.js
@@ -52,22 +52,44 @@ module.exports = function(passport){
 	// LOGIN FACEBOOK
 	router.get('/login/facebook',
 		passport.authenticate('facebook', {
-			scope : ['public_profile', 'user_friends', 'email', 'user_events', 'user_location' ]
+			scope : ['public_profile', 'user_friends', 'user_events', 'email' ]
 		}));
 // RETURN AFTER LOGIN FB
 router.get('/login/facebook/return', 
 	passport.authenticate('facebook', {
-		failureRedirect: '/login/facebook', // set to login/facebook for security
+		failureRedirect: '/login/facebook' // set to login/facebook for security
 	}),
 	function(req, res) {
 		res.redirect('/profile');
 	});
 // GO TO PROFILE
+
+// FACEBOOK API ETC
+
+var Facebook = require('facebook-node-sdk');
+
+var facebook = new Facebook( { 
+	appID: process.env.CLIENT_ID, 
+	secret: process.env.CLIENT_SECRET 
+});
+
+// PROFILE FB
 router.get('/profile',
-	require('connect-ensure-login').ensureLoggedIn(),
-	function(req, res){
-		console.log("USER TESTING")
-		console.log(req.user)
+	require('connect-ensure-login').ensureLoggedIn(), function(req, res) {
+
+
+		console.log("############### profile loaded ###################")
+
+		facebook.setAccessToken(req.user.accessToken)
+
+		facebook.api( '/me/friends', function(err, res) {
+			if(!res || res.error) {
+				console.log(!res ? 'error occurred' : res.error);
+				return;
+			}
+			console.log(res)
+		});
+
 
 		res.render('profile', { user: req.user });
 	});
