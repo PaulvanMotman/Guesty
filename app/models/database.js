1015d309c36d0fdd34accee1fcd91b61cb4c1e54
Sven van Hoorn <svenvanhoorn@gmail.com>
2016-06-17 16:18:05 +0200

    changed serialize and desirialize in facebook.js and changes the /home route

--- app/models/database.js
+++ app/models/database.js
@@ -6,9 +6,9 @@ var db = {
 // Set up sql
 var Sequelize = require( 'sequelize' )
 db.conn = new Sequelize('guesty', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-	// host: '192.168.99.100',
-	// port: '32772',
-	host: 'localhost',
+	host: '192.168.99.100',
+	port: '32768',
+	// host: 'localhost',
 	dialect: 'postgres'
 });
 
@@ -40,7 +40,7 @@ db.mainuser.hasMany(db.event);
 db.event.belongsTo(db.mainuser);
 
 // Synchronise with database
-db.conn.sync( {'force': true} ).then( 
+db.conn.sync( {'force': false} ).then( 
 	() => { console.log ( 'Sync succeeded' ) },
 	( err ) => { console.log('sync failed: ' + err) } 
 	)
