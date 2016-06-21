ad9047ab80c8e8154acab498075cb5b3d1d5c511
Paul van Motman <pcvanmotman@gmail.com>
2016-06-21 17:56:52 +0200

    added divs to each guest on the guestlist

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,10 +1,17 @@
 $( document ).ready(function() {
 	$('.checkbox').change(function() {
-  		var guest = {guest: $(this).parent().text(), fbeventid: $("#fbeventid").text(), ajax: true}
-  		console.log(guest)
-  		$.get( '/save', guest, function(data) {
-  			console.log("Saved the following guest in database: " + data)
-  		})
+		var guest = {guest: $(this).parent().text(), fbeventid: $("#fbeventid").text(), ajax: true}
+		$.get( '/save', guest, function(data) {
+			console.log("Saved the following guest in database: " + data)
+		})
+		console.log('change triggered')
+		if($(this).is(":checked"))   {
+			console.log('check is true ')
+			$(this).nextAll(".guestinfo").show();
+		} else{
+			console.log('check is false ')
+			$(this).nextAll(".guestinfo").hide();
+		}
 	});
 });
 
