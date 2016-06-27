0e8050af6699f6a7cdc05128e575920ed326bf7f
Sven van Hoorn <svenvanhoorn@gmail.com>
2016-06-27 13:14:46 +0200

    added heroku postgres DB and made guesty run on heroku server

--- app/models/database.js
+++ app/models/database.js
@@ -5,13 +5,27 @@ var db = {
 
 // Set up sql
 var Sequelize = require( 'sequelize' )
-db.conn = new Sequelize('guesty', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-	// host: '192.168.99.100',
-	// port: '32768',
-	host: 'localhost',
-	dialect: 'postgres'
-});
 
+if (process.env.DATABASE_URL) {
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	  if (err) throw err;
+	  console.log('Connected to postgres! Getting schemas...');
+
+	  client
+	    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+	    .on('row', function(row) {
+	      console.log(JSON.stringify(row));
+	    });
+	});
+} else {
+	db.conn = new Sequelize('guesty', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+		host: '192.168.99.100',
+		port: '32768',
+		// host: 'localhost',
+		dialect: 'postgres'
+});
+}
 
 
 //// Models
